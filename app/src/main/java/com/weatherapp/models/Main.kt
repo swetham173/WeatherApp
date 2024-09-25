bb78bdd0e557f5efe041f857d453ed68192aa3d9
swetham173 <swetham173@gmail.com>
2024-09-25 09:15:22 +0530

    Initial commit of weather app project

---
+++ app/src/main/java/com/weatherapp/models/Main.kt
@@ -0,0 +1,11 @@
+package com.weatherapp.models
+
+import java.io.Serializable
+
+data class Main(
+    val temp: Double,
+    val pressure: Int,
+    val humidity: Int,
+    val temp_min: Double,
+    val temp_max: Double
+) : Serializable
